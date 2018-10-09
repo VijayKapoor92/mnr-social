0d1938d17180dfe8a413c7f7ebe87fb21aba3aa5
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-08 22:39:21 -0300

    feat: Adiciona constante para quando houver logo

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,4 @@
 export const
-    MNR_LOGO = "http://sistema.mercadonarede.com.br/imagens/logo.png"
+    MNR_LOGO = "http://sistema.mercadonarede.com.br/imagens/logo.png",
+    MNR_NO_LOGO = "https://mbtskoudsalg.com/images/no-logo-png-6.png"
 ;
