d244ab86cf0f93f6d5eccca3d1efb7c46413ea60
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-09 00:42:14 -0300

    feat: Implementa novas props

--- src/App.js
+++ src/App.js
@@ -2,9 +2,45 @@ import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            iconInfoOn: false,
+            focusSuggestionsField: false,
+            hoverSuggestionsField: false,
+            blurSuggestionsField: false
+        }
+    }
+
+    handleIconOnOff = () =>
+        this.setState({iconInfoOn: !this.state.iconInfoOn});
+
+    handleFocusSuggestionField = focus =>
+        this.setState({focusSuggestionsField: focus});
+
+    /*handleBlurSuggestionsField = blur =>
+        this.setState({blurSuggestionsField: blur});*/
+
+    handleMouseLeaveSuggestionsField = () =>
+        this.setState({hoverSuggestionsField: false});
+
+    handleMouseOverSuggestionsField = () =>
+        this.setState({hoverSuggestionsField: true});
+
     render() {
+        const { iconInfoOn, focusSuggestionsField, hoverSuggestionsField } = this.state;
+
         return (
-            <NavBar/>
+            <NavBar
+                onChangeIcon={this.handleIconOnOff}
+                iconInfoOn={iconInfoOn}
+                onFocusSuggestionField={this.handleFocusSuggestionField}
+                onBlurSuggestionsField={this.handleFocusSuggestionField}
+                focusSuggestionsField={focusSuggestionsField}
+                onMouseOverSuggestionsField={this.handleMouseOverSuggestionsField}
+                onMouseLeaveSuggestionsField={this.handleMouseLeaveSuggestionsField}
+                hoverSuggestionsField={hoverSuggestionsField}
+            />
         )
     }
 }
