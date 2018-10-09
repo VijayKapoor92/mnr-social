1700d1d04fd8b256ebefa2f5bd012efb86b6c45e
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-09 00:42:43 -0300

    refactor: Adiciona novas props e estilos

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 import {
     AppBar,
     Toolbar,
@@ -10,32 +11,44 @@ import {
     ShareOutlined,
     PersonAdd
 } from '@material-ui/icons';
-import ToggleIcon from 'material-ui-toggle-icon';
-import Spacer from '../Spacer';
-import {MNR_LOGO} from "../../constants";
+import SuggestionsField from '../SuggestionField';
+import {MNR_NO_LOGO} from "../../constants";
 import {withStyles} from '@material-ui/core/styles';
 import styles from './jscss';
 
-const NavBar = ({classes}) =>
-    <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar>
-            <Tooltip title="Info" placement="bottom">
-                <IconButton color="inherit">
-                    <ContactPhone />
-                </IconButton>
-            </Tooltip>
-            <img src={MNR_LOGO} alt="logo" className={classes.logo}/>
-            <Spacer />
-            <Tooltip title="Compartilhe sua página" placement="bottom">
-                <IconButton color="inherit">
-                    <ShareOutlined/>
-                </IconButton>
-            </Tooltip>
-            <Tooltip title="Cadastre-se" placement="bottom">
-                <IconButton color="inherit">
-                    <PersonAdd/>
-                </IconButton>
-            </Tooltip>
+const NavBar = ({classes, onChangeIcon, onFocusSuggestionField, onBlurSuggestionsField, focusSuggestionsField, onMouseOverSuggestionsField, onMouseLeaveSuggestionsField, hoverSuggestionsField}) =>
+    <AppBar position="fixed" className={classes['app-bar']}>
+        <Toolbar className={classes.toolbar}>
+            <div className={classes['app-bar-left']}>
+                <Tooltip title="Info" placement="bottom">
+                    <IconButton color="inherit" onClick={() => onChangeIcon()}>
+                        <ContactPhone/>
+                    </IconButton>
+                </Tooltip>
+                <img src={MNR_NO_LOGO} alt="logo" className={classNames(classes.logo, MNR_NO_LOGO && classes['no-logo'])}/>
+            </div>
+            <div className={classes['app-bar-center']}>
+                <SuggestionsField
+                    onFocusSuggestionField={onFocusSuggestionField}
+                    onBlurSuggestionsField={onBlurSuggestionsField}
+                    onMouseOverSuggestionsField={onMouseOverSuggestionsField}
+                    onMouseLeaveSuggestionsField={onMouseLeaveSuggestionsField}
+                    focus={focusSuggestionsField}
+                    hover={hoverSuggestionsField}
+                />
+            </div>
+            <div className={classes['app-bar-right']}>
+                <Tooltip title="Compartilhe sua página" placement="bottom">
+                    <IconButton color="inherit">
+                        <ShareOutlined/>
+                    </IconButton>
+                </Tooltip>
+                <Tooltip title="Cadastre-se" placement="bottom">
+                    <IconButton color="inherit">
+                        <PersonAdd/>
+                    </IconButton>
+                </Tooltip>
+            </div>
         </Toolbar>
     </AppBar>
 ;
--- src/components/NavBar/jscss.js
+++ src/components/NavBar/jscss.js
@@ -1,9 +1,34 @@
 export default ({
-    appBar: {
+    'app-bar': {
         backgroundColor: '#283752'
+    },
+    'app-bar-left': {
+        flexGrow: 1,
+        display: 'inline-flex',
+        justifyContent: 'flex-start',
+        alignContent: 'center'
+    },
+    'app-bar-center': {
+        flexGrow: 1,
+        display: 'inline-flex',
+        justifyContent: 'center',
+        alignContent: 'center'
+    },
+    'app-bar-right': {
+        flexGrow: 1,
+        display: 'inline-flex',
+        justifyContent: 'flex-end',
+        alignContent: 'center'
+    },
+    toolbar: {
+        justifyContent: 'space-between'
     },
     logo: {
         width: 50,
-        height: 32
+        height: 32,
+        alignSelf: 'center'
+    },
+    'no-logo': {
+        borderRadius: '50%'
     }
 });
