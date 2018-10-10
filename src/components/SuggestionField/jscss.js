864229f43914c6c95035a5ef6234e0664ddc5d29
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-09 22:00:14 -0300

    styles: Cria icon, icon-dark-grey, loader-out, loader-in e root

--- src/components/SuggestionField/jscss.js
+++ src/components/SuggestionField/jscss.js
@@ -5,7 +5,7 @@ export default ({
         padding: '5px 10px',
         backgroundColor: 'rgba(900, 900, 900, 0.1)',
         borderRadius: 20,
-        transition: 'background-color 250ms ease-out'
+        transition: 'background-color 300ms linear'
     },
     'textfield-hover': {
         backgroundColor: 'rgba(900, 900, 900, 0.2)'
@@ -13,8 +13,24 @@ export default ({
     'textfield-focus': {
         backgroundColor: 'rgba(900, 900, 900, 1)'
     },
+    icon: {
+        transition: 'color 300ms linear'
+    },
+    'icon-dark-grey': {
+        color: grey[800]
     },
     'icon-default': {
         color: grey[500]
+    },
+    'loader-out': {
+        opacity: 0,
+        transition: 'opacity 500ms'
+    },
+    'loader-in': {
+        opacity: 1
+    },
+    root: {
+        position: 'relative',
+        width: '100%'
     }
 });
