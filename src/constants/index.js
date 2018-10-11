52affbf99e8c229f96692b00e159f45ae6e8f84e
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-11 19:17:41 -0300

    feat: Muda constants para utils

--- src/constants/index.js
+++
@@ -1,4 +0,0 @@
-export const
-    MNR_LOGO = "http://sistema.mercadonarede.com.br/imagens/logo.png",
-    MNR_NO_LOGO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVX4RgUYvaDyHQaEiejmjMy0ZbuEPqGkOwsxq9oAmPl3MQJIRC"
-;
---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const
+    MNR_LOGO = "http://sistema.mercadonarede.com.br/imagens/logo.png",
+    MNR_NO_LOGO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVX4RgUYvaDyHQaEiejmjMy0ZbuEPqGkOwsxq9oAmPl3MQJIRC",
+    MNR_CADASTRO_COMPRADOR_EMPRESAS = "http://www.mercadonarede.com.br/index.php/pesquisa-de-cnpjcpf/",
+    MNR_CADASTRO_COMPRADOR_CONCESSIONARIAS = "http://www.mercadonarede.com.br/index.php/comprador-concessionarias-lojas-e-distribuidores-de-autopecas/",
+    MNR_CADASTRO_FORNECEDOR = "http://www.mercadonarede.com.br/index.php/fornecedor/"
+;
