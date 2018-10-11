6204f4b4cc7f9d58e6aa4b759af5ede24cae4995
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-11 19:12:58 -0300

    style: Adiciona novos estilos ao navbar

--- src/components/NavBar/jscss.js
+++ src/components/NavBar/jscss.js
@@ -1,3 +1,4 @@
+import {blue} from '@material-ui/core/colors';
 export default ({
     'app-bar': {
         backgroundColor: '#283752'
@@ -30,5 +31,40 @@ export default ({
     },
     'no-logo': {
         borderRadius: '50%'
+    },
+    paper: {
+        width: 280,
+        padding: 10
+    },
+    paperCadastro: {
+        width: 280,
+        padding: 10,
+        height: 350
+    },
+    textfield: {
+        width: '95%',
+        backgroundColor: 'rgba(224, 224, 224, 1)',
+        padding: '0 5px',
+        borderRadius: 30,
+        '&:hover': {
+            backgroundColor: 'rgba(224, 224, 224, .6)'
+        }
+    },
+    input: {
+        fontSize: 14
+    },
+    button: {
+        borderColor: blue[500],
+        color: blue[500],
+        '&:hover': {
+            backgroundColor: 'rgba(33, 150, 243, .2)',
+        },
+    },
+    smallLabel: {
+        fontSize: 12
+    },
+    rightIcon: {
+        marginLeft: 8,
+        fontSize: 18
     }
 });
