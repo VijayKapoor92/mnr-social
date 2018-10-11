1989037d9e86aacda18e11725306d4656bc20e6d
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-11 19:15:19 -0300

    refactor: Componente só abre atraves de um curto circuito

--- src/components/Suggestions/index.js
+++ src/components/Suggestions/index.js
@@ -1,53 +1,47 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import classNames from 'classnames';
 import {
     Paper,
     List,
     ListItem,
     ListItemText,
-    Avatar
+    Avatar,
 } from '@material-ui/core';
 import Business from '@material-ui/icons/Business';
+import Launch from '@material-ui/icons/Launch';
 import styles from './jscss';
 import {withStyles} from '@material-ui/core/styles';
-
-import {suggestions} from '../../utils/suggestions';
-
+//TODO: MUDAR PARA CONST A DECLARACAO DO COMPONENTE
 export default withStyles(styles)(
     class Suggestions extends Component {
-        constructor(props){
-            super(props);
-            this.state = {
-                suggestions: []
-            }
-        }
-
-        componentWillMount(){
-            this.setState({suggestions: suggestions})
-        }
-
         render() {
-            const { classes, query } = this.props;
+            const { classes, query, focus, suggestions } = this.props;
             const open = query.length > 0;
+            const hasSuggestions = suggestions.length > 0;
+
             return (
-                <Paper className={classNames(classes['suggestions-container'], open && classes['suggestions-container-opened'])}>
-                    {suggestions && suggestions.length > 0 && suggestions.map(({id, primary, secondary}) => (
-                        <List key={id} className={classNames(classes.list, open && classes['list-visible'])}>
-                            <ListItem>
-                                <Avatar>
-                                    <Business/>
-                                </Avatar>
-                                <ListItemText
-                                    primary={primary}
-                                    secondary={secondary}
-                                    primaryTypographyProps={{
-                                        noWrap: true
-                                    }}
-                                />
-                            </ListItem>
-                        </List>
-                    ))}
-                </Paper>
+                <Fragment>
+                    {open && suggestions && hasSuggestions && focus && (
+                        <Paper className={classes['suggestions-container']}>
+                            <List>
+                                {suggestions.map(({id, primary, secondary}) => (
+                                    <ListItem key={id} className={classes.listItem}>
+                                        <Avatar>
+                                            <Business/>
+                                        </Avatar>
+                                        <ListItemText
+                                            primary={primary}
+                                            secondary={secondary}
+                                            primaryTypographyProps={{
+                                                noWrap: true
+                                            }}
+                                        />
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </Paper>
+                    )}
+                </Fragment>
             )
         }
     }
